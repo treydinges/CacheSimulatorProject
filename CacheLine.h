f5d7bd8c2220a793fd86e7ccfdefa365cd058c33
Trey Dinges <51143145+treydinges@users.noreply.github.com>
2020-04-21 17:01:28 -0500

    Create CacheLine.h

---
+++ CacheLine.h
@@ -0,0 +1,25 @@
+#ifndef CACHELINE_H
+#define CACHELINE_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class CacheLine {
+    public:
+        CacheLine();
+        CacheLine(int blockSize);
+        void dumpLine();
+        void viewLine();
+        void flushLine();
+        string decimalToHex(int decimalValue); 
+    private:
+        int B;
+        int V;
+        int D;
+        int T;
+        vector<string> data;
+};
+
+#endif
