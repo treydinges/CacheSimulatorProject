15440b16950f2b6bf0efc29592d89848abd4755f
Trey Dinges <51143145+treydinges@users.noreply.github.com>
2020-04-21 17:04:52 -0500

    Create Memory.h

---
+++ Memory.h
@@ -0,0 +1,27 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#define ADDRESS_WIDTH 8
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Memory {
+    public:
+        Memory();
+        Memory(int addressSize);
+        ~Memory();
+        void initializeMemory(string inFileName);
+        void makeEmpty();
+        void viewRam();
+        void dumpRam();
+        bool isEmpty();
+        int getSize() { return ram.size(); };
+        string decimalToHex(int decimalValue);
+    private:
+        vector<string> ram;
+};
+
+#endif
