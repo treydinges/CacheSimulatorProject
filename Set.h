ca7aa112b311a0d3cb99107f0d67e1030e952dd6
Trey Dinges <51143145+treydinges@users.noreply.github.com>
2020-04-21 17:05:10 -0500

    Create Set.h

---
+++ Set.h
@@ -0,0 +1,24 @@
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+#include <vector>
+
+#include "CacheLine.h"
+
+using namespace std;
+
+class Set {
+    public:
+        Set();
+        Set(int associativity, int blockSize);
+        void dumpSet();
+        void viewSet();
+        void flushSet();
+    private:
+        int E; // number of lines per set (associativity)
+        int B; // for creation of the CacheLine
+        vector<CacheLine> lines;
+};
+
+#endif
