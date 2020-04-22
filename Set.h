9538ab76c06deacce1beab6af2a1ea191433d582
Trey Dinges <51143145+treydinges@users.noreply.github.com>
2020-04-22 18:52:11 -0500

    Update Set.h

--- Set.h
+++ Set.h
@@ -12,10 +12,14 @@ class Set {
     public:
         Set();
         Set(int associativity, int blockSize);
-        bool Contains(int tag);
         void dumpSet();
         void viewSet();
         void flushSet();
+        void setBlock(vector<string> block, int line);
+        void setTag(int tag, int line);
+        void setValid(int line);
+        string getByte(int line, int offset);
+        bool Contains(int tag);
     private:
         int E; // number of lines per set (associativity)
         int B; // for creation of the CacheLine
