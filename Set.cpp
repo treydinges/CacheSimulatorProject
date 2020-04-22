108e1f6b8fb2d9a9f7ccf9de236eeb533221e9bd
Trey Dinges <51143145+treydinges@users.noreply.github.com>
2020-04-22 18:52:04 -0500

    Update Set.cpp

--- Set.cpp
+++ Set.cpp
@@ -20,15 +20,6 @@ Set::Set(int associativity, int blockSize) {
     }
 }
 
-bool Set::Contains(int tag) {
-    for (int i = 0; i < E; i++) {
-        if (lines[i].Contains(tag)) {
-            return true;
-        }
-    }
-    return false;
-}
-
 void Set::dumpSet() {
     for (int i = 0; i < E; i++) {
         lines[i].dumpLine();
@@ -47,4 +38,29 @@ void Set::flushSet() {
     for (int i = 0; i < E; i++) {
         lines[i].flushLine();
     }
+}
+
+void Set::setBlock(vector<string> block, int line) {
+    lines[line].setBlock(block);
+}
+
+void Set::setTag(int tag, int line) {
+    lines[line].setTag(tag);
+}
+void Set::setValid(int line) {
+    lines[line].setValid();
+}
+
+string Set::getByte(int line, int offset) {
+    return lines[line].getByte(offset);
+}
+
+bool Set::Contains(int tag) {
+    bool contains = false;
+    for (int i = 0; i < E; i++) {
+        if (lines[i].Contains(tag)) {
+            contains = true;
+        }
+    }
+    return contains;
 }
